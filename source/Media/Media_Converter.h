4fbc5d5044892d0b73062954f444788e69849d61
Torstein <Torsrind@outlook.com>
2026-05-28 23:22:28 +0200

    Debug

---
+++ source/Media/Media_Converter.h
@@ -0,0 +1,16 @@
+//
+// Created by Torstein on 5/28/2026.
+//
+
+#ifndef MVICONVERTER_MEDIA_CONVERTER_H
+#define MVICONVERTER_MEDIA_CONVERTER_H
+
+namespace Mvi::Media
+{
+    void func()
+    {
+        
+    }
+}
+
+#endif //MVICONVERTER_MEDIA_CONVERTER_H
